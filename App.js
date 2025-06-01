cef1203a1b60c07f8d999dff84e9ab15d9475a1d
dm002314473 <dm002314473@fer.hr>
2025-06-01 16:02:08 +0200

    connected to supabase

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import "react-native-url-polyfill/auto";
+import { supabase } from "./config/supabase";
 
 export default function App() {
+  const [souvenirs, setSouvenirs] = useState([]);
+
+  useEffect(() => {
+    async function fetchSouvenirs() {
+      const { data, error } = await supabase.from("souvenirs").select("*");
+
+      if (error) {
+        console.error("Error fetching souvenirs:", error);
+      } else {
+        setSouvenirs(data);
+      }
+    }
+
+    fetchSouvenirs();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 20 }}>
+      {souvenirs.map((s) => (
+        <Text key={s.id}>
+          {s.name} - ${s.price}
+        </Text>
+      ))}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
